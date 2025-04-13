f3f06d9558f67471d07f447bd015ad29891b58a4
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-13 12:19:35 -0300

    Add: Tarefas

---
+++ tarefas.php
@@ -0,0 +1,10 @@
+<?php
+$tarefas = [
+    ["tarefa" => "Jogar video game com os amigos", "status" => "Pendente"],
+    ["tarefa" => "Estudar Java, Angular e Typescript", "status" => "Finalizada"],
+    ["tarefa" => "Finalizar o projeto de e-commerce", "status" => "Pendente"],
+    ["tarefa" => "Reunião com a equipe de desenvolvimento (10h)", "status" => "Finalizada"],
+    ["tarefa" => "Escrever o TCC sobre Redes Neurais Convolucionais", "status" => "Pendente"],
+    ["tarefa" => "Enviar relatório de lucro para o gerente do setor de vendas", "status" => "Pendente"]
+];
+?>
